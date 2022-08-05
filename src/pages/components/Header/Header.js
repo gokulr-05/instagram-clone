50dad1db4d1288ecd6bbf530e1ba0c0d35814b4e
gokulr-05 <gokulravivcr05@gmail.com>
2022-08-05 12:23:27 +0530

    header completed

--- src/pages/components/Header/Header.js
+++ src/pages/components/Header/Header.js
@@ -1,11 +1,14 @@
 import React from "react";
 import "./header.css";
-
 import logo from "../../../assets/img.png";
+
+import pic1 from "../../../assets/pic1.jpg";
+// import pic2 from "../../../assets/pic2.jpg";
+// import pic3 from "../../../assets/pic3.jpg";
 
 const Header = () => {
   return (
-    <div className="header-area">
+    <div className="header-area sticky-top">
       <div className="header-sec ">
         {/* header row  */}
         <div className="header-row row">
@@ -15,7 +18,7 @@ const Header = () => {
           </div>
 
           {/* search bar  */}
-          <div className="header-searchbar-area d-none d-sm-block  col-sm-3">
+          <div className="header-searchbar-area d-none d-md-block  col-md-3">
             <div className="header-search-bar row">
               <div className="m-0 p-0  col-1">
                 <i class="fa-solid fa-magnifying-glass text-muted header-search-icon"></i>
@@ -32,13 +35,33 @@ const Header = () => {
           </div>
 
           {/* menu icons  */}
-          <div className="header-menu-area col-6 col-sm-6">
-            <div className="header-menu-icons col-2"></div>
-            <div className="header-menu-icons col-2"></div>
-            <div className="header-menu-icons col-2"></div>
-            <div className="header-menu-icons col-2"></div>
-            <div className="header-menu-icons col-2"></div>
-            <div className="header-menu-icons col-2"></div>
+          <div className="header-menu-area col-8 col-md-6">
+            <div className="header-menu-container-row row ">
+              <div className="header-menu-container  col-xl-8 col-sm-10 col-12">
+                <div className="header-menu-sec row">
+                  <div className="header-menu-icons col-2 col-lg-2">
+                    <i class="fa-solid fa-house fs-4 m-0"></i>
+                  </div>
+                  <div className="header-menu-icons col-2 col-lg-2">
+                    <i class="fa-brands fa-facebook-messenger fs-4"></i>
+                  </div>
+                  <div className="header-menu-icons col-2 col-lg-2">
+                    <i class="fa-solid fa-square-plus fs-4"></i>
+                  </div>
+                  <div className="header-menu-icons col-2 col-lg-2">
+                    <i class="fa-solid fa-compass fs-4"></i>
+                  </div>
+                  <div className="header-menu-icons col-2 col-lg-2">
+                    <i class="fa-solid fa-heart fs-4"></i>
+                  </div>
+                  <div className="header-menu-icons  col-2 col-lg-2">
+                    <div className="header-profile-img-container">
+                      <img src={pic1} className="header-profile-img" alt="" />
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
